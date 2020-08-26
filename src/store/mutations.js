c65fd892510c9b405a10e51edafd493efbebdfbe
kirtioking <106045174@qq.com>
2020-08-26 21:31:15 +0800

    后台管理系统

---
+++ src/store/mutations.js
@@ -0,0 +1,14 @@
+export default {
+    //修改state菜单
+    REQ_MENULIST(state,payload){
+        state.menuList = payload
+    },
+    //修改state角色
+    REQ_ROLELIST(state,payload){
+        state.roleList = payload
+    },
+    //修改state管理员
+    REQ_USERLIST(state,payload){
+        state.userList = payload
+    }
+}
